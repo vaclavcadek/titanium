d4244f73c98a366d831d40b9ae11a210dc775c79
Václav Čadek <vaclavcadek@gmail.com>
2016-11-01 16:54:28 +0100

    Proof-of-concept of tree evaluator

--- titanium/__init__.py
+++ titanium/__init__.py
@@ -12,12 +12,17 @@ SUPPORTED_NS = {
     '4.3': 'http://www.dmg.org/PMML-4_3'
 }
 
+SUPPORTED_MODELS = [
+    'NeuralNetwork',
+    'TreeModel'
+]
+
 
 class PMMLVersionNotSupportedException(Exception):
     pass
 
 
-def read_pmml(file):
+def read_pmml(file, model='NeuralNetwork'):
     tree = ET.parse(file)
     pmml = tree.getroot()
     version = pmml.attrib.get('version', None)
@@ -25,29 +30,49 @@ def read_pmml(file):
     if None in [version, ns]:
         msg = 'Unsupported version of PMML.\nSupported versions are: {}'.format(SUPPORTED_NS.keys())
         raise PMMLVersionNotSupportedException(msg)
-    nn = pmml.find('{}:NeuralNetwork'.format(version), SUPPORTED_NS)
-    neural_inputs = nn.find('{}:NeuralInputs'.format(version), SUPPORTED_NS).findall('{}:NeuralInput'.format(version),
-                                                                                     SUPPORTED_NS)
-    norms = []
-    for ni in neural_inputs:
-        nc = ni.find('{}:DerivedField'.format(version), SUPPORTED_NS).find('{}:NormContinuous'.format(version),
-                                                                           SUPPORTED_NS)
-        ln = nc.findall('{}:LinearNorm'.format(version), SUPPORTED_NS)
-        x0 = float(ln[0].attrib['orig'])
-        y0 = float(ln[0].attrib['norm'])
-        x1 = float(ln[1].attrib['orig'])
-        y1 = float(ln[1].attrib['norm'])
-        norms.append(lambda x, x0=x0, y0=y0, x1=x1, y1=y1: y0 + (x - x0) / (x1 - x0) * (y1 - y0))
-    layers = nn.findall('{}:NeuralLayer'.format(version), SUPPORTED_NS)
-    weights = []
-    biases = []
-    activations = [getattr(activation, l.attrib['activationFunction']) for l in layers]
-    for l in layers:
-        neurons = l.findall('{}:Neuron'.format(version), SUPPORTED_NS)
-        biases.append(np.array([float(n.attrib['bias']) for n in neurons]))
-        W = []
-        for n in neurons:
-            connections = n.findall('{}:Con'.format(version), SUPPORTED_NS)
-            W.append([float(c.attrib['weight']) for c in connections])
-        weights.append(np.array(W))
-    return evaluation.FullyConnectedNeuralNetworkEvaluator(weights, biases, activations, norms)
+
+    if model == 'NeuralNetwork':
+        nn = pmml.find('{}:NeuralNetwork'.format(version), SUPPORTED_NS)
+        neural_inputs = nn.find('{}:NeuralInputs'.format(version), SUPPORTED_NS).findall(
+            '{}:NeuralInput'.format(version),
+            SUPPORTED_NS)
+        norms = []
+        for ni in neural_inputs:
+            nc = ni.find('{}:DerivedField'.format(version), SUPPORTED_NS).find('{}:NormContinuous'.format(version),
+                                                                               SUPPORTED_NS)
+            ln = nc.findall('{}:LinearNorm'.format(version), SUPPORTED_NS)
+            x0 = float(ln[0].attrib['orig'])
+            y0 = float(ln[0].attrib['norm'])
+            x1 = float(ln[1].attrib['orig'])
+            y1 = float(ln[1].attrib['norm'])
+            norms.append(lambda x, x0=x0, y0=y0, x1=x1, y1=y1: y0 + (x - x0) / (x1 - x0) * (y1 - y0))
+        layers = nn.findall('{}:NeuralLayer'.format(version), SUPPORTED_NS)
+        weights = []
+        biases = []
+        activations = [getattr(activation, l.attrib['activationFunction']) for l in layers]
+        for l in layers:
+            neurons = l.findall('{}:Neuron'.format(version), SUPPORTED_NS)
+            biases.append(np.array([float(n.attrib['bias']) for n in neurons]))
+            W = []
+            for n in neurons:
+                connections = n.findall('{}:Con'.format(version), SUPPORTED_NS)
+                W.append([float(c.attrib['weight']) for c in connections])
+            weights.append(np.array(W))
+        return evaluation.FullyConnectedNeuralNetworkEvaluator(weights, biases, activations, norms)
+    elif model == 'TreeModel':
+        decision_tree = pmml.find('{}:TreeModel'.format(version), SUPPORTED_NS)
+        mining_schema = decision_tree.find('{}:MiningSchema'.format(version), SUPPORTED_NS)
+        mining_fields = mining_schema.findall('{}:MiningField'.format(version), SUPPORTED_NS)
+        root = decision_tree.find('{}:Node'.format(version), SUPPORTED_NS)
+        fields = [f.attrib['name'] for f in mining_fields if f.attrib.get('usageType', None) != 'predicted']
+
+        def get_children(node):
+            return node.findall('{}:Node'.format(version), SUPPORTED_NS)
+
+        def get_predicate(node):
+            return node.find('{}:SimplePredicate'.format(version), SUPPORTED_NS)
+
+        def get_score_distributions(node):
+            return node.findall('{}:ScoreDistribution'.format(version), SUPPORTED_NS)
+
+        return evaluation.TreeModelEvaluator(fields, root, get_children, get_predicate, get_score_distributions)
--- titanium/evaluation.py
+++ titanium/evaluation.py
@@ -24,3 +24,42 @@ class FullyConnectedNeuralNetworkEvaluator:
     def predict_classes(self, X):
         probs = self.predict_proba(X)
         return np.argmax(probs, 1)
+
+
+class TreeModelEvaluator:
+    def __init__(self, fields, root, get_children, get_predicate, get_score_distributions):
+        self._fields = fields
+        self._root = root
+        self._get_children = get_children
+        self._get_predicate = get_predicate
+        self._get_score_distributions = get_score_distributions
+
+    def predict_proba(self, X):
+        node = self._root
+        while True:
+            children = self._get_children(node)
+            # is leaf
+            if len(children) == 0:
+                break
+            else:
+                p = self._get_predicate(children[0])
+                field = p.attrib['field']
+                operator = p.attrib['operator']
+                split_point = float(p.attrib['value'])
+                val = X[self._fields.index(field)]
+                condition = {
+                    'greaterThan': val > split_point,
+                    'greaterOrEqual': val >= split_point,
+                    'lessThan': val < split_point,
+                    'lessOrEqual': val <= split_point,
+                }[operator]
+                if condition:
+                    node = children[0]
+                else:
+                    node = children[1]
+        rc = [float(c.attrib['recordCount']) for c in self._get_score_distributions(node)]
+        return np.array(rc) / np.sum(rc)
+
+    def predict_classes(self, X):
+        probs = self.predict_proba(X)
+        return np.argmax(probs, 1)
